3dd8a2f69e1850774192fbd171f3323daa63b5ff
Pgrm97 <Pgrm97@gmail.com>
2020-11-14 00:39:13 -0400

    Added the Home Component layout.

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, View, Text } from 'react-native';
+import { Card } from 'react-native-elements'
+import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
+function RenderItem(props) {
+    const item = props.item;
+
+    if (item != null){
+        return(
+            <Card>
+                <Card.Title>{item.name}</Card.Title>
+                <Card.FeaturedSubtitle>{item.designation}</Card.FeaturedSubtitle>
+                <Card.Image source={{uri:
+                    'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg'}}/>
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    else{
+        return(
+            <View></View>
+        );
+    }
+}
 
 class Home extends Component {
 
     constructor(props){
         super(props);
+        this.state = {
+            dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
+        }
     }
 
     static navigationOptions = {
@@ -13,9 +45,11 @@ class Home extends Component {
 
     render(){
         return(
-            <View>
-                <Text>Home Component</Text>
-            </View>
+            <ScrollView>
+                <RenderItem item={this.state.dishes.filter((dish) => dish.featured)[0]}/>
+                <RenderItem item={this.state.promotions.filter((promotion) => promotion.featured)[0]}/>
+                <RenderItem item={this.state.leaders.filter((leader) => leader.featured)[0]}/>
+            </ScrollView>
         );
     }
 }
